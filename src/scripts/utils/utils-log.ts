6d68f56d89b87f71599b8e8b1212878a43bd6477
Nils <nils.yseboodt@gmail.com>
2024-07-21 12:03:44 +0200

    fix: can't access private field

--- src/scripts/utils/utils-log.ts
+++ src/scripts/utils/utils-log.ts
@@ -9,61 +9,67 @@ const resetFormat: Required<Omit<FormattedString, 'message'>> = {
   color: 'reset',
 }
 
+function build(logFunc: Function, ...args: FormattedString[]) {
+  const messageParts: string[] = [];
+  const styles: string[] = [];
+  for (let arg of args) {
+    if (typeof arg === 'string') {
+      arg = {message: arg}
+    }
+    arg = {
+      ...resetFormat, // reset any values not provided
+      ...arg,
+    };
+
+    messageParts.push(`%c${arg.message}`);
+    styles.push(`color: ${arg.color};`);
+  }
+
+  return logFunc.bind(console, messageParts.join(' '), ...styles);
+}
+
 export class UtilsLog {
   
+
+  public info: Console['info'];
+  public debug: Console['debug'];
+  public log: Console['log'];
+  public warn: Console['warn'];
+  public error: Console['error'];
+
   readonly #package: UtilsPackage.Package;
   constructor(pack: UtilsPackage.Package) {
     UtilsPackage.requireInternalCaller();
     this.#package = pack;
+    this.info = this.#createWithPrefix('info');
+    this.debug = this.#createWithPrefix('debug');
+    this.log = this.#createWithPrefix('log');
+    this.warn = this.#createWithPrefix('warn');
+    this.error = this.#createWithPrefix('error');
   }
 
   public buildInfo(...args: FormattedString[]): (message?: any, ...optionalParams: any[]) => void {
-    return UtilsLog.#build(console.info, {message: this.#package.id, color: '#ff8f00'}, ...args);
+    return build(console.info, {message: this.#package.id, color: '#ff8f00'}, ...args);
   }
   public buildDebug(...args: FormattedString[]): (message?: any, ...optionalParams: any[]) => void {
-    return UtilsLog.#build(console.debug, {message: this.#package.id, color: '#ff8f00'}, ...args);
+    return build(console.debug, {message: this.#package.id, color: '#ff8f00'}, ...args);
   }
   public buildLog(...args: FormattedString[]): (message?: any, ...optionalParams: any[]) => void {
-    return UtilsLog.#build(console.log, {message: this.#package.id, color: '#ff8f00'}, ...args);
+    return build(console.log, {message: this.#package.id, color: '#ff8f00'}, ...args);
   }
   public buildWarn(...args: FormattedString[]): (message?: any, ...optionalParams: any[]) => void {
-    return UtilsLog.#build(console.warn, {message: this.#package.id, color: '#ff8f00'}, ...args);
+    return build(console.warn, {message: this.#package.id, color: '#ff8f00'}, ...args);
   }
   public buildError(...args: FormattedString[]): (message?: any, ...optionalParams: any[]) => void {
-    return UtilsLog.#build(console.error, {message: this.#package.id, color: '#ff8f00'}, ...args);
+    return build(console.error, {message: this.#package.id, color: '#ff8f00'}, ...args);
   }
-
-  public info = this.#createWithPrefix('info');
-  public debug = this.#createWithPrefix('debug');
-  public log = this.#createWithPrefix('log');
-  public warn = this.#createWithPrefix('warn');
-  public error = this.#createWithPrefix('error');
 
   #createWithPrefix<T extends keyof Console>(key: T): Console[T] {
     if (typeof console[key] === 'function') {
-      return UtilsLog.#build(console[key], {message: this.#package.id, color: '#ff8f00'});
+      return build(console[key], {message: this.#package.id, color: '#ff8f00'});
     } else {
       return console[key];
     }
-  }
-
-  static #build(logFunc: Function, ...args: FormattedString[]) {
-    const messageParts: string[] = [];
-    const styles: string[] = [];
-    for (let arg of args) {
-      if (typeof arg === 'string') {
-        arg = {message: arg}
-      }
-      arg = {
-        ...resetFormat, // reset any values not provided
-        ...arg,
-      };
-
-      messageParts.push(`%c${arg.message}`);
-      styles.push(`color: ${arg.color};`);
-    }
-
-    return logFunc.bind(console, messageParts.join(' '), ...styles);
   }
 
 }
