f6d28c9a99da7898cf014489875dd15728e3a50e
Nils <nils.yseboodt@gmail.com>
2024-08-21 02:49:29 +0200

    fix: type conflict with league types

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,5 @@
 export * from './module-scope.js';
 export { ComponentConfig, AttributeConfig, EventConfig, OutputConfig, OnInit, OnInitParam } from './render-engine/component.js';
 export * from './utils/stoppable.js';
-export * from './types/foundry.js';
-export * from './types/foundry-globals.js';
 export * from './types/html-data.js';
 export * from './types/lib-wrapper.js';
